dbc2c757021ff955b5bb0827577eac8c8dc5781e
David Ekpo <jofesdavid@gmail.com>
2026-03-29 21:31:15 +0100

    files renaming

--- app/settings.py
+++ app/settings.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '.env'))
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), 'utils', '.env'))
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
