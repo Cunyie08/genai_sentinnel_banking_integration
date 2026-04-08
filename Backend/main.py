fd837d1a163b14012052d7c6ad421c65082a5325
David Ekpo <jofesdavid@gmail.com>
2026-04-08 18:51:26 +0100

    host correction

--- Backend/main.py
+++ Backend/main.py
@@ -1803,5 +1803,5 @@ async def get_faqs(prompt: Optional[str] = None):
             "searched_for": prompt
         }
 if __name__ == "__main__":
-    # uvicorn.run(app, host='0.0.0.0', port=8080)
-      uvicorn.run(app, host='127.0.0.1', port=8080)
+    uvicorn.run(app, host='0.0.0.0', port=8080)
+    # uvicorn.run(app, host='127.0.0.1', port=8080)
